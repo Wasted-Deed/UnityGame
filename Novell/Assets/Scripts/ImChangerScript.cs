fdfd6c9846953b6076ccb94b5ee5fbc2fedc095c
Spilrobe <ya.moya19@mail.ru>
2020-10-02 17:27:30 +0300

    Added menu and game scenes + buttons

---
+++ Novell/Assets/Scripts/ImChangerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImChangerScript : MonoBehaviour
+{
+    
+    private int frameIndex;
+    public Sprite[] sprites;
+    [SerializeField] private Image imChanger;
+
+ public void Start()
+ {
+     imChanger.sprite = sprites[0];
+ }
+ public void FixedUpdate()
+ {
+      Debug.Log(frameIndex);
+      imChanger.sprite = sprites[frameIndex];
+ }
+ public void NextFrame()
+  {
+     
+     if (frameIndex < sprites.Length-1)
+     {
+       
+      frameIndex ++;
+      
+     }
+     else
+      frameIndex = 0;
+         
+  }
+}
---
+++ Novell/Assets/Scripts/MenuControllerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControllerScript : MonoBehaviour
+{
+    public void StartPressed()
+  {
+     SceneManager.LoadScene(1);
+  }
+
+  public void ExitPressed()
+  {
+     Application.Quit();
+  }
+}
